9243c1191e0b52909d7f19a0a8005d86c498b551
Lucas André <105679596+eulucasandre@users.noreply.github.com>
2023-06-27 18:51:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraCarro();
+  mostraAtor();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
